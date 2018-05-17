8f17a66c2b89e9600612de30d57ea0ae883540f4
Raja Swathi <sr16e@my.fsu.edu>
2018-05-16 23:54:19 -0400

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -37,7 +38,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
